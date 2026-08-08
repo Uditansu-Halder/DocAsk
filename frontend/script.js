4eef5c41c7b8768e4d7b9017907b91def0e55b2c
Code-Dark-Knight <uditansuhalder7131@gmail.com>
2026-08-08 22:35:39 +0530

    Updated the backend url

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "https://docask-2bka.onrender.com";
 
 async function uploadPDF() {
     const fileInput = document.querySelector("#pdfFile");
